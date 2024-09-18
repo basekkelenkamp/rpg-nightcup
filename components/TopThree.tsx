5b88d9f195d23695f6626c82fd3f2aaa7df5f0a1
Bas <57452503+basekkelenkamp@users.noreply.github.com>
2024-09-18 21:55:11 +0200

    fix topthree on mobile

--- components/TopThree.tsx
+++ components/TopThree.tsx
@@ -1,4 +1,3 @@
-// /components/TopThree.tsx
 'use client';
 
 import { FaMedal } from 'react-icons/fa';
@@ -7,10 +6,10 @@ const medalColors = ['text-yellow-400', 'text-gray-400', 'text-amber-600'];
 
 const TopThree = ({ players }: { players: string[] }) => {
   return (
-    <div className="flex items-end justify-center mt-8 space-x-6">
+    <div className="flex flex-wrap items-end justify-center mt-8 space-x-6 sm:space-x-4 max-w-full overflow-hidden">
       {/* Second Place */}
-      <div className="flex flex-col items-center transform transition duration-500 hover:scale-110">
-        <div className="bg-base-300 p-2 rounded-t-lg text-center w-36 border border-gray-500">
+      <div className="flex flex-col items-center transform transition duration-500 hover:scale-110 w-24 sm:w-32 md:w-36">
+        <div className="bg-base-300 p-2 rounded-t-lg text-center w-full border border-gray-500">
           <span className="text-lg font-semibold">{players[1]}</span>
         </div>
         <div className="bg-base-200 w-full h-6 rounded-b-lg flex items-center justify-center border-x border-b border-gray-500">
@@ -20,8 +19,8 @@ const TopThree = ({ players }: { players: string[] }) => {
       </div>
 
       {/* First Place */}
-      <div className="flex flex-col items-center transform transition duration-500 hover:scale-110">
-        <div className="bg-base-300 p-2 rounded-t-lg text-center w-42 border border-yellow-400">
+      <div className="flex flex-col items-center transform transition duration-500 hover:scale-110 w-28 sm:w-40 md:w-42">
+        <div className="bg-base-300 p-2 rounded-t-lg text-center w-full border border-yellow-400">
           <span className="text-xl font-bold">{players[0]}</span>
         </div>
         <div className="bg-base-200 w-full h-8 rounded-b-lg flex items-center justify-center border-x border-b border-yellow-400">
@@ -31,8 +30,8 @@ const TopThree = ({ players }: { players: string[] }) => {
       </div>
 
       {/* Third Place */}
-      <div className="flex flex-col items-center transform transition duration-500 hover:scale-110">
-        <div className="bg-base-300 p-2 rounded-t-lg text-center w-32 border border-amber-600">
+      <div className="flex flex-col items-center transform transition duration-500 hover:scale-110 w-20 sm:w-28 md:w-32">
+        <div className="bg-base-300 p-2 rounded-t-lg text-center w-full border border-amber-600">
           <span className="text-lg font-semibold">{players[2]}</span>
         </div>
         <div className="bg-base-200 w-full h-4 rounded-b-lg flex items-center justify-center border-x border-b border-amber-600">
